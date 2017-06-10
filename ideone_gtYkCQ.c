5a165fc9b2f4a57b550583349e974f350a32ceeb
nasrinirfana <irfanajumana@gmail.com>
2017-06-10 13:35:35 +0530

    Add files via upload

---
+++ ideone_gtYkCQ.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+  
+  scanf("%d",&n);
+  if(n==0)
+  printf("zero");
+  else if(n>0)
+  printf("positive");
+   else if(n>0)
+  printf("negative");
+  else 
+  return 0;
+	
+	return 0;
+}
